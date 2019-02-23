ad80dfc0a502c2381d7229d4665eb40993fe1284
NieLamu <mxmxlty@gmail.com>
2019-02-23 15:35:06 +0800

    Tags in post and index pages are Linked to archive page

--- source/js/main.js
+++ source/js/main.js
@@ -4,26 +4,4 @@ $(document).ready(function(){
     console.log('%c 🎯 Material-T', logStyle)
     console.log('%c 🏷 Version: 0.9.1 ', logStyle)
     console.log('%c 📦 https://github.com/invom/Material-T ', logStyle) 
-
-    const hash = window.location.hash;
-    const pathname = window.location.pathname;
-    if (pathname.includes('archives') && hash){
-        console.log(hash, $('.tab-pane'));
-        $('.card-body .tab-pane').each(function(){
-            let ele = $(this);
-            if ('#' + ele.attr('id') === hash){
-                ele.addClass('active show');
-            }else{
-                ele.removeClass('active show');
-            }
-        });
-        $('.card-body .nav-link').each(function(){
-            let ele = $(this);
-            if (ele.attr('href') === hash){
-                ele.addClass('active show');
-            }else{
-                ele.removeClass('active show');
-            }
-        });
-    }
 }) 
